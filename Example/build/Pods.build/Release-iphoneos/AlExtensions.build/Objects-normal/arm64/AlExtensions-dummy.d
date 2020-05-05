36e3b4658b542560b556fbffc3f288f201975fe7
Soner Güler <soner.guler@applogist.com>
2020-05-05 17:44:03 +0300

    Doc add

---
+++ Example/build/Pods.build/Release-iphoneos/AlExtensions.build/Objects-normal/arm64/AlExtensions-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/sonerguler/Applogist/Frameworks/AL-Extensions/Example/Pods/Target\ Support\ Files/AlExtensions/AlExtensions-dummy.m \
+  /Users/sonerguler/Applogist/Frameworks/AL-Extensions/Example/build/Pods.build/Release-iphoneos/AlExtensions.build/module.modulemap \
+  /Users/sonerguler/Applogist/Frameworks/AL-Extensions/Example/Pods/Target\ Support\ Files/AlExtensions/AlExtensions-prefix.pch
---
+++ Example/build/Pods.build/Release-iphoneos/AlExtensions.build/Objects-normal/armv7/AlExtensions_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/sonerguler/Applogist/Frameworks/AL-Extensions/Example/build/Pods.build/Release-iphoneos/AlExtensions.build/DerivedSources/AlExtensions_vers.c
