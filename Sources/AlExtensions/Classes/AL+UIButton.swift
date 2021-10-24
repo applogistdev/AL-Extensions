4b3eb2bd633b0b878ba39e23ebdcd4d84eda7027
Ahmet Sina Ustem <asend.ustem@gmail.com>
2021-10-24 18:41:40 +0300

    Added new extensions to UIButton

---
+++ Sources/AlExtensions/Classes/AL+UIButton.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+public extension UIButton {
+    private var states: [UIControl.State] {
+        return [.normal, .selected, .highlighted, .disabled]
+    }
+
+    /// Set image for all states.
+    ///
+    /// - Parameter image: UIImage.
+    func setImageForAllStates(_ image: UIImage) {
+        states.forEach { setImage(image, for: $0) }
+    }
+
+    /// Set title color for all states.
+    ///
+    /// - Parameter color: UIColor.
+    func setTitleColorForAllStates(_ color: UIColor) {
+        states.forEach { setTitleColor(color, for: $0) }
+    }
+
+    /// Set title for all states.
+    ///
+    /// - Parameter title: title string.
+    func setTitleForAllStates(_ title: String) {
+        states.forEach { setTitle(title, for: $0) }
+    }
+}
